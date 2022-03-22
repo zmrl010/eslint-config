f0d9cf32cd66ff67f67321652716681935f2dd32
Zach <zmriel@gmail.com>
2022-03-22 08:26:51 -0400

    more default rules

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   extends: [
+    "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "plugin:prettier/recommended",
   ],
 };
